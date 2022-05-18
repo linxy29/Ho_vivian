28a8f5672a84abc88cacc3c391e20082d4d4c917
linxy29 <xinyi_lin@outlook.com>
2022-05-18 11:00:44 +0800

    cluster the ncc sample

--- ncc_get_seuratInfo.R
+++ ncc_get_seuratInfo.R
@@ -10,13 +10,13 @@ pathSPC = "/storage/holab/linxy/vivian/veloAE/"  ## for server
 options(future.globals.maxSize = 40000 * 1024^2)
 
 
-ncc_seuratObj = readRDS("/storage/holab/linxy/vivian/seuratObj/2021-11-09_NCC.rds")
-DimPlot(ncc_seuratObj, reduction = "umap", label = T)
+load("/storage/holab/linxy/vivian/seuratObj/clustered_ncc.rdata")
+DimPlot(ncc, reduction = "umap", label = T)
 
 
-head(Cells(ncc_seuratObj))
-write.csv(Cells(ncc_seuratObj), file = str_c(pathSPC, "ncc_cellID_obs.csv"), row.names = FALSE)
-head(Embeddings(ncc_seuratObj, reduction = "umap"))
-write.csv(Embeddings(ncc_seuratObj, reduction = "umap"), file = str_c(pathSPC, "ncc_cell_embeddings.csv"))
-head(Idents(ncc_seuratObj))
-write.csv(Idents(ncc_seuratObj), file = str_c(pathSPC, "ncc_clusters.csv"))
+head(Cells(ncc))
+write.csv(Cells(ncc), file = str_c(pathSPC, "ncc_cellID_obs.csv"), row.names = FALSE)
+head(Embeddings(ncc, reduction = "umap"))
+write.csv(Embeddings(ncc, reduction = "umap"), file = str_c(pathSPC, "ncc_cell_embeddings.csv"))
+head(Idents(ncc))
+write.csv(Idents(ncc), file = str_c(pathSPC, "ncc_clusters.csv"))
